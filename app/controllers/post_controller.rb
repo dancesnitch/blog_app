2cd1a6be944877abecbb99fdb8e8cc8bfeb76b9a
Daniel Byrne <byrne.daniel87@gmail.com>
2017-05-13 22:12:44 +0100

    added routes to post controller, variables to index

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -1,4 +1,25 @@
 class PostController < ApplicationController
 	def index
+		@content_first = 'This is some sample text for our Awesome Ruby Blog!'
+		@content_second = 'This is more Content for our Aesome new Ruby Blog!'
+	end
+
+	def new
+	end
+
+	def create
+	end
+
+	def edit
+	end
+
+	def update
+	end
+
+	def show
+	end
+
+	def destroy
 	end
 end
+	
